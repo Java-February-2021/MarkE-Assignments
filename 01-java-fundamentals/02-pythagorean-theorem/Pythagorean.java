dcdfc4c14e432e5c7292cb2449443b21e65b2675
Mark Enders <endersma@gmail.com>
2021-02-17 23:42:22 -0800

    finished assignment. hopefully correct repository now

---
+++ 01-java-fundamentals/02-pythagorean-theorem/Pythagorean.java
@@ -0,0 +1,6 @@
+import java.lang.*;
+public class Pythagorean {
+    public double calculateHypotenuse(int legA, int legB) {
+        return Math.sqrt((legA * legA) + (legB * legB));
+    }
+}
---
+++ 01-java-fundamentals/02-pythagorean-theorem/PythagoreanTest.java
@@ -0,0 +1,6 @@
+public class PythagoreanTest{
+    public static void main(String[] args) {
+        Pythagorean calc = new Pythagorean();
+        System.out.println(calc.calculateHypotenuse(2, 4));
+    }
+}
