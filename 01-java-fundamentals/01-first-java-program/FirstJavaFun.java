8f8608b9d84df47731d5d005954e5f93242f28a0
Mark Enders <endersma@gmail.com>
2021-02-17 23:40:54 -0800

    finished assignment. hopefully pushing to the right repository

---
+++ 01-java-fundamentals/01-first-java-program/FirstJavaFun.java
@@ -0,0 +1,7 @@
+public class FirstJavaFun {
+    public static void main(String[] args) {
+        System.out.println("My name is Mark Enders");
+        System.out.println("I am 39 years old");
+        System.out.println("My hometown is Seattle, WA");
+    }
+}
